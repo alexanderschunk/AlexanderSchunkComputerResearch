b0857a1413b9f77bd3448f1477b0e077eb500fde
alexanderschunk <36488018+alexanderschunk@users.noreply.github.com>
2018-02-20 15:13:25 +0100

    Add files via upload

---
+++ alexanderschunkTestMeltdownUndSpectre1ForTest1.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: Alexander
+ * Datum: 19.02.2018
+ * Zeit: 15:09
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+
+namespace AlexanderSchunkMeltdownUndSpectreDirectXTest1
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		//Alexander Schunk Meltdown Test DirectX und Direct3D für Windows 2018
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			MessageBox.Show("Alexander Schunk Meltdown Test 3D", "Meltdown Test 3D", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+			
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
